6c6c7604bd45bc319d0ba54cb8a8cfa008262eeb
Raúl Rodríguez Torres <alu0101047287@ull.edu.es>
2020-08-09 14:29:10 +0100

    Sonar: Solucionado code smell app.e2e-spec.ts

--- Pruebas/Angular-Back/bananat/e2e/src/app.e2e-spec.ts
+++ Pruebas/Angular-Back/bananat/e2e/src/app.e2e-spec.ts
@@ -1,109 +1,87 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 import {by, element} from 'protractor';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-
-
-    var username, password, loginButton;
 
 describe('workspace-project App', () => {
-	let page: AppPage;
 
 	beforeEach(() => {
-		page = new AppPage();
+		new AppPage();
+    });
 
-         });
+	let usertest = {
+        name:   'Manolo',
+        last:   'Cool',
+        email:  'manolocool@gmail.com',
+        pass:   'manolo',
+        username: 'Manolo'
+    }
+    /*let receiver = {
+        name:   'End2End Receiver',
+        last:   'pepito',
+        email:  'e2ereceiver@netchat.com',
+        pass:   'e2etesting',
+        username: 'receiver'
+    }*/
 
-	var usertest = {
-                        name:   'Manolo',
-                        last:   'Cool',
-                        email:  'manolocool@gmail.com',
-                        pass:   'manolo',
-                        username: 'Manolo'
-                },
-
-                receiver = {
-                        name:   'End2End Receiver',
-                        last:   'pepito',
-                        email:  'e2ereceiver@netchat.com',
-                        pass:   'e2etesting',
-                        username: 'receiver'
-
-                },
-          path = require( 'path' );
-          browser.get('http://localhost:4200/');
-
+    browser.get('http://localhost:4200/');
 
 	describe('Test básicos', function() {
-
-		it('El titulo de la plataforma', function() {
-			expect(browser.getTitle()).toEqual('Bananat');
-		});
-
+        it('El titulo de la plataforma', function() {
+            expect(browser.getTitle()).toEqual('Bananat');
+        });
 
 		it('Url al abrir la aplicación', function() {
-			expect( browser.getCurrentUrl()).toEqual('http://localhost:4200/login');
+			expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/login');
 	  	});
 
 		it ( 'Prueba del nombre de la etiqueta nombre', function () {
-
-                        browser.get('http://localhost:4200/register' );
-                        var  labelName = element(by.name('name'));
-                        expect(labelName.getText()).toBe('');
-                });
-
-
+            browser.get('http://localhost:4200/register' );
+            let labelName = element(by.name('name'));
+            expect(labelName.getText()).toBe('');
+        });
 	});
 
-	 describe('Test formulario de inicio de sesión', function() {
-		browser.get('http://localhost:4200/');
+    describe('Test formulario de inicio de sesión', function() {
+        browser.get('http://localhost:4200/');
 
-		it('Comprobar el encabezado h1 de la pagina login',function(){
+        it('Comprobar el encabezado h1 de la pagina login',function(){
             browser.get('http://localhost:4200/login');
-            var h1element = element(by.css('.appName'));
-
+            let h1element = element(by.css('.appName'));
             expect(h1element.getText()).toBe('BANANAT');
         });
 
 	    it('La existencia de un formulario de login',function(){
             browser.get('http://localhost:4200/login');
-            var formulario = element(by.id('userForm'));
-
+            let formulario = element(by.id('userForm'));
             expect(formulario.isPresent()).toBe(true);
         });
 
         it('La existencia de un campo email',function(){
             browser.get('http://localhost:4200/login');
-            var em = element(by.id('user_field'));
-
+            let em = element(by.id('user_field'));
             expect(em.isPresent()).toBe(true);
         });
 
         it('La existencia de un campo contraseña',function(){
             browser.get('http://localhost:4200/login');
-            var pw = element(by.id('password_field'));
-
+            let pw = element(by.id('password_field'));
             expect(pw.isPresent()).toBe(true);
         });
 
         it('Boton de loguearse',function(){
             browser.get('http://localhost:4200/login');
-            var btn = element(by.name('Login'));
-
+            let btn = element(by.name('Login'));
             expect(btn.isPresent()).toBe(true);
             expect(btn.getText()).toBe('INICIAR SESIÓN');
         });
 
-         it('Redirección a registro',function(){
+        it('Redirección a registro',function(){
             browser.get('http://localhost:4200/login');
-            var cuenta = element(by.id('registerOption'));
-
+            let cuenta = element(by.id('registerOption'));
             expect(cuenta.isPresent()).toBe(true);
             expect(element(by.linkText('¿No tiene cuenta aún? Regístrese')).getTagName()).toBe('a');
-
             element(by.linkText('¿No tiene cuenta aún? Regístrese')).click();
             browser.sleep(4000);
-
             expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/register' );
         });
 
@@ -111,168 +89,156 @@ describe('workspace-project App', () => {
 	});
 
 	describe('Test formulario de Registro', function() {
-
         it('Comprobar el encabezado h1 de la pagina register',function(){
             browser.get('http://localhost:4200/register');
-            var h1element = element(by.css('.appName'));
+            let h1element = element(by.css('.appName'));
             expect(h1element.getText()).toBe('BANANAT');
         });
 
 		it('La existencia de un formulario de register',function(){
             browser.get('http://localhost:4200/register');
-            var formulario = element(by.css('.register .formulario'));
+            let formulario = element(by.css('.register .formulario'));
             expect(formulario.isPresent()).toBe(true);
         });
 
 		it('La existencia de un campo nombre',function(){
             browser.get('http://localhost:4200/register');
-            var nom = element(by.id('first_name'));
+            let nom = element(by.id('first_name'));
             expect(nom.isPresent()).toBe(true);
         });
 
 		it('La existencia de un campo apellidos',function(){
-                        browser.get('http://localhost:4200/register');
-                        var ape = element(by.id('surname'));
-                        expect(ape.isPresent()).toBe(true);
-                });
+            browser.get('http://localhost:4200/register');
+            let ape = element(by.id('surname'));
+            expect(ape.isPresent()).toBe(true);
+        });
 
 		it('La existencia de un campo email',function(){
             browser.get('http://localhost:4200/register');
-            var em = element(by.id('email'));
+            let em = element(by.id('email'));
             expect(em.isPresent()).toBe(true);
         });
 
-
         it('La existencia de un campo contraseña',function(){
             browser.get('http://localhost:4200/register');
-            var pw = element(by.id('password'));
+            let pw = element(by.id('password'));
             expect(pw.isPresent()).toBe(true);
 		});
 
-		 it('La existencia de un campo Nombre de usuario',function(){
+		it('La existencia de un campo Nombre de usuario',function(){
             browser.get('http://localhost:4200/register');
-            var nu = element(by.id('username'));
+            let nu = element(by.id('username'));
             expect(nu.isPresent()).toBe(true);
         });
 
 		it('Boton de registrarse',function(){
             browser.get('http://localhost:4200/register');
-            var btn = element(by.name('action'));
+            let btn = element(by.name('action'));
             expect(btn.isPresent()).toBe(true);
         });
 
 		it('Boton de registrarse mediante google',function(){
             browser.get('http://localhost:4200/register');
-            var btnG = element(by.css('.googleRegister'));
+            let btnG = element(by.css('.googleRegister'));
             expect(btnG.isPresent()).toBe(true);
             expect(btnG.getText()).toBe('Google');
         });
 
 		it('Boton de registrarse mediante Facebook',function(){
             browser.get('http://localhost:4200/register');
-            var btnF = element(by.css('.facebookRegister'));
+            let btnF = element(by.css('.facebookRegister'));
             expect(btnF.isPresent()).toBe(true);
             expect(btnF.getText()).toBe('Facebook');
         });
-
 	});
 
 	describe('Test para incluir iconos en cada campo', function() {
-
     	it('Icono de usuario en login',function(){
             browser.get('http://localhost:4200/login');
-            var icp = element(by.id('il-mensaje'));
-
+            let icp = element(by.id('il-mensaje'));
 			expect(icp.isPresent()).toBe(true);
         });
 
-		it('Icono de password  en login',function(){
+		it('Icono de password en login',function(){
             browser.get('http://localhost:4200/login');
-            var icc = element(by.id('il-candado'));
-
+            let icc = element(by.id('il-candado'));
             expect(icc.isPresent()).toBe(true);
         });
 
 		it('Icono de usuario en register',function(){
             browser.get('http://localhost:4200/register');
-            var icpe = element(by.id('ir-persona'));
-
+            let icpe = element(by.id('ir-persona'));
             expect(icpe.isPresent()).toBe(true);
         });
 
 		it('Icono de password en register',function(){
             browser.get('http://localhost:4200/register');
-            var icpass = element(by.id('ir-candado'));
+            let icpass = element(by.id('ir-candado'));
             expect(icpass.isPresent()).toBe(true);
         });
 
 		it('Icono de correo en register',function(){
             browser.get('http://localhost:4200/register');
-            var iccor = element(by.id('ir-mensaje'));
+            let iccor = element(by.id('ir-mensaje'));
             expect(iccor.isPresent()).toBe(true);
         });
 
 		it('Icono de Nombre de usuario  en register',function(){
             browser.get('http://localhost:4200/register');
-            var icnu = element(by.id('ir-nu'));
+            let icnu = element(by.id('ir-nu'));
             expect(icnu.isPresent()).toBe(true);
         });
 
 		it('Icono de google en register',function(){
             browser.get('http://localhost:4200/register');
-            var icG = element(by.css('.GIcon'));
+            let icG = element(by.css('.GIcon'));
             expect(icG.isPresent()).toBe(true);
         });
 
 		it('Icono de facebook  en register',function(){
             browser.get('http://localhost:4200/register');
-            var icF = element(by.css('.fbIcon'));
+            let icF = element(by.css('.fbIcon'));
             expect(icF.isPresent()).toBe(true);
         });
-
-
-
 	});
 
-	  describe('Test de registro e inicio de sesión', function() {
+	describe('Test de registro e inicio de sesión', function() {
 /*
                 it ('fallo de  login',function(){
                         browser.get('http://localhost:4200/login');
                         username.sendKeys('admin');
                         password.sendKeys('fail');
                         loginButton.click().then( function(){
-                                var popup= element(by.css('.popup-container.error-popup'));
+                                let popup= element(by.css('.popup-container.error-popup'));
                                 expect(popup.isDisplayed()).toBeTruthy();
                         });
                 });
 */
-                it ('Crear un usuario de prueba', function () {
-                        browser.get('http://localhost:4200/register' );
-                        element( by.id( 'first_name' ) ).sendKeys(usertest.name );
-                        element( by.id( 'surname' ) ).sendKeys(usertest.last );
-                        element( by.id( 'password' ) ).sendKeys( usertest.pass );
-                        element( by.id( 'email' ) ).sendKeys(usertest.email );
-                        element( by.id( 'username') ).sendKeys( usertest.username );
-                        element( by.buttonText( 'Registrarse' ) ).click();
-                        browser.sleep(2000);
+        it ('Crear un usuario de prueba', function () {
+            browser.get('http://localhost:4200/register' );
+            element( by.id( 'first_name' ) ).sendKeys(usertest.name );
+            element( by.id( 'surname' ) ).sendKeys(usertest.last );
+            element( by.id( 'password' ) ).sendKeys( usertest.pass );
+            element( by.id( 'email' ) ).sendKeys(usertest.email );
+            element( by.id( 'username') ).sendKeys( usertest.username );
+            element( by.buttonText( 'Registrarse' ) ).click();
+            browser.sleep(2000);
+            expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/login' );
+        });
 
-                        expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/login' );
-            });
 
-
-                it ('Comprobar el usuario creado',function() {
-                        browser.get('http://localhost:4200/login');
-                        element( by.id( 'user_field' ) ).sendKeys(usertest.email);
-                        element( by.id( 'password_field' ) ).sendKeys(usertest.pass);
-                        element( by.buttonText( 'Iniciar Sesión' ) ).click();
-			browser.sleep(5000);
-                        expect(browser.getCurrentUrl()).toBe('http://localhost:4200/chat');
-                        expect(element(by.name('Logout')).isPresent()).toBe(true);
-                        element(by.name('Logout')).click();
-                        browser.sleep(4000);
-                         expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/login' );
-
-                });
+        it ('Comprobar el usuario creado',function() {
+            browser.get('http://localhost:4200/login');
+            element( by.id( 'user_field' ) ).sendKeys(usertest.email);
+            element( by.id( 'password_field' ) ).sendKeys(usertest.pass);
+            element( by.buttonText( 'Iniciar Sesión' ) ).click();
+            browser.sleep(5000);
+            expect(browser.getCurrentUrl()).toBe('http://localhost:4200/chat');
+            expect(element(by.name('Logout')).isPresent()).toBe(true);
+            element(by.name('Logout')).click();
+            browser.sleep(4000);
+            expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/login' );
+        });
 
                 //it ('Borrar un usuario', function () {
                        //db.removeUser(usertest.usr);
@@ -286,35 +252,28 @@ describe('workspace-project App', () => {
 
                 //expect(browser.getCurrentUrl()).toBe('http://localhost:4200/chat');
         // });
-
-        });
-
-
+    });
 
 	describe('Test de la página chat', function(){
 		it ('Cerrar usuario tras iniciar sesión',function() {
-
-                        browser.get('http://localhost:4200/login');
-			browser.sleep(4000);
-                        element( by.id( 'user_field' ) ).sendKeys(usertest.email);
-                        element( by.id( 'password_field' ) ).sendKeys(usertest.pass);
-                        element( by.buttonText( 'Iniciar Sesión' ) ).click();
-			browser.sleep(4000);
-                        expect(browser.getCurrentUrl()).toBe('http://localhost:4200/chat');
-			 var lOut = element(by.css('.logout'));
-		         expect(lOut.isPresent()).toBe(true);
-			 expect(element(by.name('Logout')).isPresent()).toBe(true);
-		         element(by.name('Logout')).click();
-            	        browser.sleep(4000);
-           		 expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/login' );
-
-
-                });
-
+            browser.get('http://localhost:4200/login');
+            browser.sleep(4000);
+            element( by.id( 'user_field' ) ).sendKeys(usertest.email);
+            element( by.id( 'password_field' ) ).sendKeys(usertest.pass);
+            element( by.buttonText( 'Iniciar Sesión' ) ).click();
+            browser.sleep(4000);
+            expect(browser.getCurrentUrl()).toBe('http://localhost:4200/chat');
+            let lOut = element(by.css('.logout'));
+            expect(lOut.isPresent()).toBe(true);
+            expect(element(by.name('Logout')).isPresent()).toBe(true);
+            element(by.name('Logout')).click();
+            browser.sleep(4000);
+            expect( browser.getCurrentUrl() ).toBe( 'http://localhost:4200/login' );
+        });
 /*
        		it('Cerrar sesión',function(){
             browser.get('http://localhost:4200/chat');
-            var lOut = element(by.css('.logout'));
+            let lOut = element(by.css('.logout'));
             expect(lOut.isPresent()).toBe(true);
         });
 
@@ -330,10 +289,9 @@ describe('workspace-project App', () => {
 	});
 
 	afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-	    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-	    expect(logs).not.toContain(jasmine.objectContaining({
-	    	level: logging.Level.SEVERE,
-	    } as logging.Entry));
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE,
+        } as logging.Entry));
 	});
 });
